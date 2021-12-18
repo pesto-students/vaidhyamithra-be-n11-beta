9bb3ab2d75aa748b36ee302c1ef0d883aa7d8f71
Manirathnam.Chiliveru <m.chiliveru@comcentrica.it>
2021-12-18 22:30:01 +0100

    optimizing search query

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -5,29 +5,40 @@ exports.search = async (req, res) => {
   const page = parseInt(req.body.pageNumber);
   const limit = parseInt(req.body.pageSize);
   const skipIndex = (page - 1) * limit;
-  const results = {};
   try {
-    let searchResults = await Blog.aggregate([
+    let results = await Blog.aggregate([
       {
         $search: {
-          index: 'blogIndex',
+          index: "blogIndex",
           text: {
             query: searchText,
             path: {
-              'wildcard': '*'
-            }
-          }
-        }
-      }
-    ])
-      .sort({ _id: 1 })
-      .limit(limit)
-      .skip(skipIndex)
-      .exec();
-    let totalCount = await Blog.find({}).count();
-    results.searchResults = searchResults;
-    results.totalCount = totalCount;
-    res.status(200).send(results);
+              wildcard: "*",
+            },
+          },
+        },
+      },
+      {
+        $facet: {
+          paginatedResults: [{ $skip: skipIndex }, { $limit: limit }],
+          totalCount: [{ $count: "count" }],
+        },
+      },
+      {
+        $addFields: {
+          total: {
+            $ifNull: [{ $arrayElemAt: ["$totalCount.count", 0] }, 0],
+          },
+        },
+      },
+      {
+        $project: {
+          paginatedResults: 1,
+          total: 1,
+        },
+      },
+    ]);
+    res.status(200).send(results[0]);
   } catch (error) {
     res.status(500).send({
       message: error.message || "Error occured",
